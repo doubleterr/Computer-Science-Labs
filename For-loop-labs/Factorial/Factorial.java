dc7d721dec60b81e879be7789b55c71612fed4c0
doubleterr <doubleterr@users.noreply.github.com>
2016-10-26 13:56:47 -0500

    Add files via upload

---
+++ For-loop-labs/Factorial/Factorial.java
@@ -0,0 +1,19 @@
+package Factorial;
+
+public class Factorial{
+    
+    public void CalcFactorial(int num){
+        
+        int factorial = 1;
+        
+        for(int i = 1; i <= num; i++){
+            
+           factorial *= i; 
+            
+        }
+        
+        System.out.println("factorial of " + num + " is " + factorial + "\n");
+        
+    }
+    
+}
---
+++ For-loop-labs/Factorial/FactorialRunner.java
@@ -0,0 +1,25 @@
+package Factorial;
+
+public class FactorialRunner{
+    
+    public static void main(String[] args){
+        
+        Factorial calc = new Factorial();
+        
+        calc.CalcFactorial(5);
+        
+        calc.CalcFactorial(4);
+        
+        calc.CalcFactorial(8);
+        
+        calc.CalcFactorial(15);
+        
+        calc.CalcFactorial(6);
+        
+        calc.CalcFactorial(9);
+        
+        calc.CalcFactorial(3);
+        
+    }
+    
+}
