7ef21be6a4b1a47ab2a040674999363212100bb0
doubleterr <doubleterr@users.noreply.github.com>
2016-10-26 18:14:22 -0500

    Add files via upload

---
+++ For-loop-labs/BinaryToTen/TwoToTenRunner.java
@@ -0,0 +1,31 @@
+package BinaryToTen;
+
+public class TwoToTenRunner{
+    
+    public static void main(String[] args){
+        
+        TwoToTen convert = new TwoToTen();
+        
+        convert.ToBinary("1010");
+        
+        convert.ToBinary("1100");
+        
+        convert.ToBinary("1110");
+        
+        convert.ToBinary("1111");
+        
+        convert.ToBinary("11111");
+        
+        convert.ToBinary("111111");
+        
+        convert.ToBinary("1110101");
+        
+        convert.ToBinary("10101010101");
+        
+        convert.ToBinary("1010101010110");
+        
+        convert.ToBinary("111111111111111111");
+        
+    }
+    
+}
