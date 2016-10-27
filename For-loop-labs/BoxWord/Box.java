e4cca62ffc92204311e9b56e5ae688ee2c402e8c
doubleterr <doubleterr@users.noreply.github.com>
2016-10-26 20:16:26 -0500

    Add files via upload

---
+++ For-loop-labs/BoxWord/Box.java
@@ -0,0 +1,15 @@
+package BoxWord;
+
+public class Box{
+    
+    public void CreateBox(String word){
+        
+        for(int x = 0; word.length() > x; x++)
+            
+            System.out.println(word);
+        
+        System.out.println("\n");
+        
+    }
+    
+}
---
+++ For-loop-labs/BoxWord/BoxRunner.java
@@ -0,0 +1,21 @@
+package BoxWord;
+
+public class BoxRunner{
+    
+    public static void main(String[] args){
+        
+        Box create = new Box();
+        
+        create.CreateBox("hippo");
+        
+        create.CreateBox("abcd");
+        
+        create.CreateBox("it");
+        
+        create.CreateBox("a");
+        
+        create.CreateBox("chicken");
+        
+    }
+    
+}
